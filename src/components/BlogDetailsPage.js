3b6f16efb8fcba345958e011e17adf8da000ce8e
Ömer Apaydın <omerapaydin90@gmail.com>
2025-03-15 18:44:43 +0300

    useParams eklendi

--- src/components/BlogDetailsPage.js
+++ src/components/BlogDetailsPage.js
@@ -1,7 +1,9 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
 const BlogDetailsPage = () => {
-  return <div>BlogDetailsPage</div>;
+  const { id } = useParams();
+  return <div>Blog Details : {id}</div>;
 };
 
 export default BlogDetailsPage;
